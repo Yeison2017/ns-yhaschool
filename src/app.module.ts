32933389ba280c3530f2966e69f4aa191251e49d
Yeison Hernandez <developer.ss.2@tucash.co>
2023-09-04 23:00:53 -0500

    Se configura setGlobalPrefix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
+import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { StudentsModule } from './students/students.module';
 import { DocumentTypeModule } from './document-type/document-type.module';
 import { SeedModule } from './seed/seed.module';
+import { ServeStaticModule } from '@nestjs/serve-static';
 
 @Module({
-  imports: [StudentsModule, DocumentTypeModule, SeedModule],
+  imports: [
+    StudentsModule,
+    DocumentTypeModule,
+    SeedModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+  ],
   controllers: [],
   providers: [],
 })
