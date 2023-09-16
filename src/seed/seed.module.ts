8047191410343d2628392633a9a24459227f4adc
Yeison Hernandez <yha.big@gmail.com>
2023-09-15 22:14:47 -0500

    Se crea un custom provider

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -4,10 +4,11 @@ import { SeedService } from './seed.service';
 import { SeedController } from './seed.controller';
 import { StudentsModule } from 'src/students/students.module';
 import { DocumentTypeModule } from 'src/document-type/document-type.module';
+import { CommonModule } from 'src/common/common.module';
 
 @Module({
   controllers: [SeedController],
   providers: [SeedService],
-  imports: [StudentsModule, DocumentTypeModule],
+  imports: [StudentsModule, DocumentTypeModule, CommonModule],
 })
 export class SeedModule {}
